3dc7e985c763a600d1424de6ef48cb42c68208d9
Max Zavaleta <mzavaletav@gmail.com>
2022-12-12 12:28:22 -0500

    feat: add put and delete methods

--- wa-sample/Controllers/CartController.cs
+++ wa-sample/Controllers/CartController.cs
@@ -7,30 +7,56 @@ namespace wa_sample.Controllers;
 [ApiController]
 [Route("cart/v1")]
 
-public class CartController: ControllerBase
+public class CartController : ControllerBase
 {
-    
+
     private DataContext _context;
 
-    public CartController(DataContext dataContext){
-        _context= dataContext;
+    public CartController(DataContext dataContext)
+    {
+        _context = dataContext;
     }
 
-    [HttpGet] 
-    public async Task<ActionResult<List<Cart>>> getList ([FromServices] DataContext context){
+    [HttpGet]
+    public async Task<ActionResult<List<Cart>>> getList([FromServices] DataContext context)
+    {
         var carts = await context.Carts.ToListAsync();
         return carts;
     }
 
     [HttpPost]
-    public async Task<ActionResult<Cart>> post ([FromServices] DataContext context, [FromBody] Cart cart){
+    public async Task<ActionResult<Cart>> post([FromServices] DataContext context, [FromBody] Cart cart)
+    {
         context.Carts.Add(cart);
         await context.SaveChangesAsync();
         return cart;
-    } 
-    [HttpGet("{id:int}")]
-    public async Task<ActionResult<Cart>> getById ([FromServices] DataContext context, int id){
+    }
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Cart>> getById([FromServices] DataContext context, int id)
+    {
         var cart = await context.Carts.FindAsync(id);
         return cart == null ? NotFound() : Ok(cart);
-    } 
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult> put([FromServices] DataContext context, int id, [FromBody] Cart cart)
+    {
+        var cartExist = await context.Carts.FindAsync(id);
+        if (cartExist == null) return NotFound();
+        context.Entry(cartExist).State = EntityState.Detached;
+
+        cart.Id = id;
+        context.Entry(cart).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+        return NoContent();
+    }
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> delete([FromServices] DataContext context, int id)
+    {
+        var cartExist = await context.Carts.FindAsync(id);
+        if (cartExist == null) return NotFound();
+        context.Carts.Remove(cartExist);
+        await context.SaveChangesAsync();
+        return NoContent();
+    }
 }
